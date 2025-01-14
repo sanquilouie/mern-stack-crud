689218e502aed421e2f52458d0a81ab9656f2021
louieeeee <louiesanqui0518@gmail.com>
2025-01-14 15:06:28 +0800

    send data to db

--- frontend/src/AddUser/AddUser.jsx
+++ frontend/src/AddUser/AddUser.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./adduser.css"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 const AddUser = () => {
+  const users = {
+    name: "",
+    email: "",
+    address: "",
+  };
+  const [user, setUser] = useState(users)
+  const navigate = useNavigate();
+
+  const inputHandler = (e) =>{
+    const {name, value}= e.target
+    console.log(name, value);
+
+    setUser({...user, [name]: value});
+  }
+
+  const submitForm = async(e)=>{
+    e.preventDefault();
+    await axios
+    .post("http://localhost:8000/api/user", user)
+    .then((response)=>{
+      console.log("User created successfully.")
+      navigate("/");
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
   return (
     <div className='addUser'>
-      <Link to="/" type='button' class='btn btn-secondary'>
-       <i class="fa-solid fa-backward"></i> Back 
+      <Link to="/" type='button' className='btn btn-secondary'>
+       <i className="fa-solid fa-backward"></i> Back 
       </Link>
       <h3>Add New User</h3>
-      <form className="addUserForm">
+      <form className="addUserForm" onSubmit={submitForm}>
         <div className='inputGroup'>
           <label htmlFor="name">Name: </label>
           <input 
           type="text"
           id="name"
+          onChange={inputHandler}
           name="name"
           autoComplete='off'
           placeholder='Enter your Name'
@@ -25,6 +54,7 @@ const AddUser = () => {
           <input 
           type="email"
           id="email"
+          onChange={inputHandler}
           name="email"
           autoComplete='off'
           placeholder='Enter your Email'
@@ -35,13 +65,14 @@ const AddUser = () => {
           <input 
           type="text"
           id="address"
+          onChange={inputHandler}
           name="address"
           autoComplete='off'
           placeholder='Enter your Address'
           />
         </div>
         <div className='inputGroup'>
-          <button type='button' class='btn btn-primary'>
+          <button type='submit' className='btn btn-primary'>
             Submit 
           </button>
         </div>
