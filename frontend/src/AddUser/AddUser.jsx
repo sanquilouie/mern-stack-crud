77b7ebb1675b5d5cdd25b95e2060c0c8788e359d
louieeeee <louiesanqui0518@gmail.com>
2025-01-15 11:01:04 +0800

    added Toast on adding user

--- frontend/src/AddUser/AddUser.jsx
+++ frontend/src/AddUser/AddUser.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import "./adduser.css"
 import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios"
+import toast from 'react-hot-toast'
 
 const AddUser = () => {
   const users = {
@@ -24,7 +25,7 @@ const AddUser = () => {
     await axios
     .post("http://localhost:8000/api/user", user)
     .then((response)=>{
-      console.log("User created successfully.")
+      toast.success(response.data.message,{ position: "top-right" });
       navigate("/");
     })
     .catch((error)=>{
